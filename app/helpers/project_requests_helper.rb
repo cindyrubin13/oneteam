1113dd0e505e34f328b556627fa507e336216696
Cindy Rubin <cindyrubin13@gmail.com>
2013-08-26 21:10:15 -0400

    javascript for user location and blank lines omitted from reports

--- app/helpers/project_requests_helper.rb
+++ app/helpers/project_requests_helper.rb
@@ -37,7 +37,35 @@ def interested_skill_score(rel_skill)
 end
 
 
-
+#def location(office_location)
+#	location_array = []
+#	 if office_location = 'Chicago'
+#	 	location_array.push(41.8819° N, 87.6278° W)
+#	 	elsif office_location = 'Mumbai'
+#	 	location_array.push(18.9750° N, 72.8258° E)
+#	 		elsif office_location = 'New York'
+#	 		location_array.push(40.6700° N, 73.9400° W)
+#				elsif office_location = 'Los Angeles'
+#	 			location_array.push(34.0500° N, 118.2500° W)
+#	 				elsif office_location = 'Atlanta'
+#	 				location_array.push(33.7550° N, 84.3900° W)
+#	 					elsif office_location = 'Detroit'
+#	 					location_array.push(42.3314° N, 83.0458° W)
+#	 						elsif office_location = 'Houston'
+#	 						location_array.push(29.7628° N, 95.3831° W)
+#	 							elsif office_location = 'Boston'
+#	 							location_array.push(42.3581° N, 71.0636° W)
+#	 							end
+#	 						end
+#						end
+#					end
+#				end
+#			end
+#		end
+#	end
+#	return location_array
+#end
+	 						
 
 
 
